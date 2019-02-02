defe153c0152c6b9b392e6f4431d54e861f836a4
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-02-02 02:24:45 -0500

    finished throught the highlighted key press step, everything working so far

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
 
 
-    $('#keyboard-upper-container').hide();
+    $('#keyboard-upper-container').hide(); //hides uppercase keyboard at page load.
 
-    $(document).keydown(function checkKeyDown(key) { //key down event listener. When a certain key is pressed the code will run.
+    $(document).keydown(function checkKeyDown(key) { //key down event listener. When the shift key (key code 16) is pressed the code runs and shows the uppercase keyboard.
         if (key.keyCode == '16') {
             $('#keyboard-upper-container').show();
         }
     });
-    $(document).keyup(function checkKeyUp(key) { //key up event listener. When a certain key is released the code will run.
+    $(document).keyup(function checkKeyUp(key) { //key up event listener. when shift key is released the uppercase keyboard is hidden again.
         if (key.keyCode == '16') {
             $('#keyboard-upper-container').hide();
         }
     });
-    $(document).keypress(function checkKeyPress(key) {
-        if (key.keyCode == '33') {
-            console.log('!');
-        } 
+    $(document).keypress(function checkKeyPress(key) { //looks for a key press, uses string concatination to look up by id and then applies css to "highlight the selected keys".
+        $('#' + key.keyCode).css({ backgroundColor: 'yellow', fontWeight: 'bold'}); 
+        
+    
+        
+        
+        
+        
+        
+    
     });
-
 
 
 
