8e7c4df3466cfeab0feb9b4f79862aad81796fdf
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-02-03 12:06:47 -0500

    at point where logic for adding next sentence works until the end of the second sentence.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ $(document).ready(function () { //waits for everything to load before running an
     let margin = 0;
     let next = 0;
     let current = 0;
+    let keyCount = 0;
+
 
     alert("When you are ready, press OK to display first line of text.") //alert that populates at page load. Once ok is clicked it appends the first string from the array.
     $('#sentence').append(sentences[0]).append('<br />');
@@ -26,11 +28,24 @@ $(document).ready(function () { //waits for everything to load before running an
         }
     });
     $(document).keypress(function checkKeyPress(key) { //looks for a key press, uses string concatination to look up by id and then applies css to "highlight the selected keys".
+        keyCount++;
+        console.log(keyCount);
+        console.log(sentences[0].length);
         $('#' + key.keyCode).css({ backgroundColor: 'yellow', fontWeight: 'bold' });
         rightWrong();
         addLeftMargin();
 
-
+//beginning of if statements used to place next sentence.. would have liked to use a loop, but had no control over when sentence was placed.
+        if (keyCount == sentences[0].length) { 
+            margin = 0;
+            $('span.glyphicon').remove();
+            $('#yellow-block').css({marginTop: '45px', marginLeft: '' + margin + 'px'});
+            margin = margin + 17;
+            $('#sentence').append(sentences[1]).append('<br />');
+            sentence = sentences[0];
+            keyCount = 0;
+        }//** */LEFT OFF HERE..... PICK THIS BACK UP**WORKS UNTIL END OF SECOND SENTENCE
+//end of if statements for placing next sentence.
 
         function addLeftMargin() { //moves the yellow box to next expected letter with every key press.
             margin = margin + 17;
@@ -38,20 +53,19 @@ $(document).ready(function () { //waits for everything to load before running an
         }
 
         function rightWrong() { //function used to compare the key pressed with the expected letter to determine if it was keyed correctly.
-            let id = $('#' + key.keyCode).text();
+            let id = $('#' + key.keyCode).text();  // *WITH THIS LOGIC, THE SPACEBAR IS NOT RECOGNIZED AS MATCHING THE BLANK SPACE IN THE EXPETED LETTER FIELD.
             next++; // moves to next letter in string
-            $('#target-letter').text(sentence[next]);
-            if(id == sentence[current]) {
+            $('#target-letter').text(sentence[next]); // keeps changing the letter in the '#target-letter field to the next expected letter.
+            
+            if (id == sentence[current]) {
                 $('#feedback').append('<span class="glyphicon glyphicon-ok"></span>');
-                console.log('right');
                 current++;
             } else {
                 $('#feedback').append('<span class="glyphicon glyphicon-remove"></span>')
-                console.log('wrong');
                 current++;
             }
-            
-            
+
+            //*WHEN FINISHED GO BACK THROUGH CODE LOOKING FOR ANYTING WRITTEN IN CAPS LIKE THIS AND CORRECT ISSUES THEN REMOVE ALL CAPS NOTATION.
 
 
 
